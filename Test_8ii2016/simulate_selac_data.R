af1a76e6d2c9d3a12585a21f22e0ef2b93a52f40
Brian O'Meara <omeara.brian@gmail.com>
2016-02-08 21:35:33 -0500

    code now starts from points corresponding to optimal.aa's codons

--- Test_8ii2016/simulate_selac_data.R
+++ Test_8ii2016/simulate_selac_data.R
@@ -3,11 +3,25 @@ library(selac)
 
 phy <- read.tree('yeast.tre')
 
-set.size <- 10
+set.size <- 2
 
 gtr.params <- runif(6, 0.1, 2)
 acgt.freq <- runif(4)
 acgt.freq <- (acgt.freq / sum(acgt.freq))[-4]
+
+numcode <- 1
+codon.sets <- selac:::CreateCodonSets()
+codon.set.translate <- apply(codon.sets, 2, n2s)
+codon.name <- apply(codon.set.translate, 1, paste, collapse="")
+codon.aa <- sapply(codon.name,selac:::TranslateCodon, numcode=numcode)
+aa.names <- unique(codon.aa)
+root.codon.freq.matrix <- matrix(0, nrow=length(aa.names), ncol=length(codon.aa))
+rownames(root.codon.freq.matrix) <- aa.names
+colnames(root.codon.freq.matrix) <- names(codon.aa)
+for (aa.index in sequence(dim(root.codon.freq.matrix)[1])) {
+	matching.codons <- which(codon.aa == rownames(root.codon.freq.matrix)[aa.index])
+	root.codon.freq.matrix[aa.index, matching.codons] <- 1/length(matching.codons)
+}
 
 for(g in 1:set.size)
 {
@@ -19,19 +33,13 @@ for(g in 1:set.size)
   model.param <- c(runif(1, 10^-10, 10^-5), runif(2, 0.01, 1), 57852168, acgt.freq, gtr.params)
   names(model.param) <- c("qphic", "alpha", "beta", "Ne", "A.freq", "C.freq", "G.freq", 
                           "GTR.alpha", "GTR.beta", "GTR.gamma", "GTR.delta", "GTR.epsilon", "GTR.eta")
-  root.codon.freq <- vector(mode = "numeric", length = 64*21)
-  for(i in 1:64)
-  {
-    x <- runif(21)
-    x <- x / sum(x)
-    root.codon.freq[((i-1)*21+1):(i*21)] <- x
-  }
+  
   
   nucl.data <- SelacSimulator(phy, pars = model.param, aa.optim_array = optim.aa, 
-                 root.codon.frequencies = root.codon.freq, numcode=1, aa.properties=NULL, 
+                 root.codon.array = root.codon.freq.matrix, numcode=1, aa.properties=NULL, 
                  nuc.model = "GTR", k.levels=0, diploid=TRUE)
   
   sim.name <- paste("gene", g, sep = "")
   write.dna(x = nucl.data, file = paste(sim.name, "fasta", sep = "."), format = "fasta", nbcol = 1, colw = 60)
-  save(list = c("model.param", "optim.aa", "root.codon.freq", "phy"), file = paste(sim.name, "Rda", sep = "."))
+  save(list = c("model.param", "optim.aa", "root.codon.freq", "phy"), file = paste(sim.name, "rda", sep = "."))
 }
